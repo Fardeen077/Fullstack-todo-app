a266bc6f16129505fc8b01e21949aa7de274a0f5
fardeen077 <fardeen36143@gmail.com>
2026-03-14 20:25:55 +0530

    add file path in index.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/index.js";
-
+import path from "path"
 const app = express();
 
+const __dirname = path.resolve();
 app.use(cors({
     origin: process.env.CORE_ORIGIN,
     credentials: true,
@@ -19,6 +20,13 @@ app.use(express.static("public"));
 app.use(cookieParser());
 app.use("/api/v1/user", userRouter);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Client/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../Client/dist", "index.html"));
+    })
+}
 app.get("/debug-cookie", (req, res) => {
     console.log(req.cookies);
     res.json(req.cookies);
