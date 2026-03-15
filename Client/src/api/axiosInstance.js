e653347936e4a1c4cf870d7d7643e272b29bc7f1
fardeen077 <fardeen36143@gmail.com>
2026-03-15 12:20:43 +0530

    testing env file is set up correctley or not

--- Client/src/api/axiosInstance.js
+++ Client/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:import.meta.env.VITE_SERVER_URL,
+    baseURL:"https://fullstack-todo-app-8.onrender.com/api/v1/user",
     withCredentials: true,
 });
--- Server/src/app.js
+++ Server/src/app.js
@@ -7,15 +7,15 @@ const app = express();
 
 const __dirname = path.resolve();
 app.use(cors({
-    origin: process.env.CORE_ORIGIN,
+    origin:"https://fullstack-todo-app-9.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.use(express.json({ limit: "2kb" }));
+app.use(express.json({ limit: "2md" }));
 
-app.use(express.urlencoded({ extended: true, limit: "2kb" }));
+app.use(express.urlencoded({ extended: true, limit: "2mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use("/api/v1/user", userRouter);
